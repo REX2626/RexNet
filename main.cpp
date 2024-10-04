e8046bac3819d9b726c1039b6d14cde2dfa4e5e9
Rex Attwood <rexattwood26@gmail.com>
2024-10-04 16:26:16 +0100

    Added window creation code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,58 @@
-int main() {
-    return 0;
+#include <stdio.h>
+#include "src/include/SDL2/SDL.h"
+
+
+
+const int WIDTH = 1000;
+const int HEIGHT = 600;
+
+int main(int argc, char* argv[]) {
+    // Initial message & SDL version
+    SDL_version version;
+    SDL_GetVersion(&version);
+    printf("Initializing PerfView (SDL version: %d.%d.%d)\n", version.major, version.minor, version.patch);
+
+    // Set up SDL
+    if (SDL_Init(SDL_INIT_VIDEO || SDL_INIT_EVENTS) < 0) {
+        printf("SDL could not initialize: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("PerfView", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_ALWAYS_ON_TOP);
+    if (window == NULL) {
+        printf("Could not create window: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    if (renderer == NULL) {
+        printf("Could not create renderer: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Event event;
+    bool running = true;
+    while (running) {
+
+        // Handle events
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+                break;
+            }
+
+            else if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_k:
+                        running = false;
+                        break;
+                }
+            }
+        }
+
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
